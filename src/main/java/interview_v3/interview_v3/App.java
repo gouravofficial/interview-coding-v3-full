e438d6cfa6a7d7be9b4701f66791d3edaf9d1c76
gourav.singh <gourav.singh@KELLGGNLPTP1126.kelltontech.org>
2023-04-14 14:57:36 +0530

    first commit

---
+++ src/main/java/interview_v3/interview_v3/App.java
@@ -0,0 +1,13 @@
+package interview_v3.interview_v3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
